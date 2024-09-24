c7d41a8ef69e8c11745d45826be00d77d2c8a1e9
Pedro Henrique Alves Hessel <pedrohenriquealveshessel@gmail.com>
2024-09-24 16:35:42 -0300

    texto agora aparece na tela e o display foi desbugado

--- PI segundo semestre/PI segundo semestre/PI segundo semestre.cpp
+++ PI segundo semestre/PI segundo semestre/PI segundo semestre.cpp
@@ -2,10 +2,13 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h> // contes TrueType
+#include <allegro5/allegro_image.h> //addon para colocar imagens
 
 int main() {
+    ALLEGRO_DISPLAY* janelaJogo = NULL; // criação da variável de display
 
-    // verifica se tudo inicializou corretamente
+    // verifica a inicilaização
     if (!al_init()) {
         fprintf(stderr, "Erro inicializando Allegro!\n");
         return -1;
@@ -13,46 +16,66 @@ int main() {
 
     if (!al_install_keyboard()) {
         fprintf(stderr, "Erro inicializando teclado!\n");
-        return
-            -1;
+        return -1;
     }
 
     if (!al_install_mouse()) {
         fprintf(stderr, "Erro inicializando mouse!\n");
-        return
-            -1;
+        return -1;
     }
 
-    // constante do tamanho da tela em pixels
-    const int WIDTH = 1920;
-    const int HEIGHT = 1080;
+    al_init_font_addon();  // inicializa o sistema de fontes
+    al_init_ttf_addon();   // inicializa o suporte para fontes TTF
 
-    // cria uma janela e armazena informação com ponteiro para uso futuro
-    ALLEGRO_DISPLAY* display = al_create_display(WIDTH, HEIGHT);
+    janelaJogo = al_create_display(800, 600); //criação da janela do jogo
+    al_set_window_position(janelaJogo, 400, 200); //posição da janela na tela
+    al_set_window_title(janelaJogo, "Tela teste"); //nome da janela
 
-    if (!display) {
+    if (!janelaJogo) {
         fprintf(stderr, "Erro inicializando display!\n");
         return -1;
-
     }
-    // cria uma fila de eventos e registra os eventos relacionados ao display
-    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
-    al_register_event_source(event_queue, al_get_display_event_source(display));
 
-    // enquanto o programa não terminar, aguarda um evento e o guarda na variavel "ev"
+    ALLEGRO_FONT* font = al_load_ttf_font("fonte.ttf", 16, 0); // carrega a fonte
+    if (!font) {
+        fprintf(stderr, "Erro carregando fonte!\n");
+        al_destroy_display(janelaJogo);
+        return -1;
+    }
+
+    // criação da fila de eventos e registra os eventos do display
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+    al_register_event_source(event_queue, al_get_display_event_source(janelaJogo));
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+    // controle do loop
     bool done = false;
+
     while (!done) {
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
 
-        // se fechar a janela o loop termina
+        // se o evento for fechar a janela
         if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             done = true;
-
         }
-    }
-    // destroi o display e a fila de eventos
-    al_destroy_display(display);
-    al_destroy_event_queue(event_queue);
 
+        // limpa a tela
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+        // desenha o texto na tela
+        al_draw_text(font, al_map_rgb(255, 255, 255), 800 / 2, 500 - 100,
+            ALLEGRO_ALIGN_CENTER, "Lorem ipsum dolor sit amet. Eos nisi rerum est facilis cupiditate a illo voluptatem et");
+
+        // atualiza o display
+        al_flip_display();
+    }
+
+
+    // destroi os recursos para poupar memoria
+    al_destroy_display(janelaJogo);
+    al_destroy_event_queue(event_queue);
+    al_destroy_font(font);
+
+    return 0;
 }
